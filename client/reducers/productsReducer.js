a79c3d4ab09d8b4c5e036fe7ec52aa93067d8b6d
mmcdevi1 <mmcdevi1@gmail.com>
2018-06-04 16:18:09 -0400

    create fetch products action creators

---
+++ client/reducers/productsReducer.js
@@ -0,0 +1,27 @@
+const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
+
+const initialState = {
+  products: [
+    { id: 1, name: 'Super Mario Bros', price: 0, inStock: 100 },
+  ]
+}
+
+export const fetchProducts = () => {
+	return dispatch => {
+		dispatch({
+			type: FETCH_PRODUCTS,
+			payload: 'res.data'
+		})
+	}
+}
+
+const reducer = (state=initialState, action) => {
+  switch (action.type) {
+    case FETCH_PRODUCTS:
+      return state;
+    default:
+      return state;
+  }
+}
+
+export default reducer;
