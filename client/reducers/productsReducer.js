15ae76d8a7aa3901969a5defd41f2f30224aac38
mmcdevi1 <mmcdevi1@gmail.com>
2018-06-04 17:24:06 -0400

    add more cases for products reducers

--- client/reducers/productsReducer.js
+++ client/reducers/productsReducer.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
 
-const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
+const REQUEST_PRODUCTS       = 'REQUEST_PRODUCTS';
+const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
+const FETCH_PRODUCTS_FAIL    = 'FETCH_PRODUCTS_FAIL';
+const DELETE_PRODUCT         = 'DELETE_PRODUCT';
+const EDIT_PRODUCT           = 'EDIT_PRODUCT';
 
 const initialState = {
   products: [
     { id: 1, name: 'Super Mario Bros', price: 0, inStock: 100 },
     { id: 2, name: 'Final Fantasy', price: 0, inStock: 100 },
-  ]
+  ],
+  isLoading: false,
+  error: false,
 }
 
 export const fetchProducts = () => {
@@ -15,7 +21,7 @@ export const fetchProducts = () => {
 			.get('/api/products')
 			.then(res => {
 				dispatch({
-					type: FETCH_PRODUCTS,
+					type: FETCH_PRODUCTS_SUCCESS,
 					payload: res.data,
 				})
 			})
@@ -27,11 +33,70 @@ export const fetchProducts = () => {
 
 const reducer = (state=initialState, action) => {
   switch (action.type) {
-    case FETCH_PRODUCTS:
-      return state;
+  	case REQUEST_PRODUCTS:
+  		return {
+  			...state,
+  			isLoading: true,
+  		}
+  	case FETCH_PRODUCTS_FAIL:
+  		return {
+  			...state,
+  			error: true,
+  		}
+    case FETCH_PRODUCTS_SUCCESS:
+      return {
+      	...state,
+      	products: action.payload,
+      	isLoading: false,
+      }
+    case NEW_PRODUCT:
+    	return {
+    		...state,
+    		products: [ ...state.products, action.payload ]
+    	}
+    case DELETE_PRODUCT:
+    	return {
+    		...state,
+    		products: state.products.filter(product => {
+    			return product.id !== action.payload
+    		})
+    	}
+    case EDIT_PRODUCT:
+    	return {
+    		...state,
+    		products: state.products.map((product, idx) => {
+    			if (product.id === action.payload.id) {
+    				return action.payload
+    			}
+    			return product
+    		})
+    	}
     default:
       return state;
   }
 }
 
 export default reducer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
