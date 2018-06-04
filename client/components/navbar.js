ad008100efb020696e5f0de5db487b8f9178d6de
mmcdevi1 <mmcdevi1@gmail.com>
2018-06-04 18:06:03 -0400

    fix store paths

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import {logout} from '../reducers/store'
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
-import {me} from './store'
+import {me} from './reducers/store'
 import Products from './components/Products';
 
 /**
