c939c4363c048734b47546a964b61d5304468d0f
iknepper <iknepper@hotmail.com>
2018-06-05 17:11:58 -0400

    made components for CartItems and Cart Item, authform changes

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -15,6 +15,16 @@ const AuthForm = (props) => {
   return (
     <div>
       <form onSubmit={handleSubmit} name={name}>
+      <div>
+          <label htmlFor="firstName"><small>First Name</small></label>
+          <input name="firstName" type="firstName" />
+        </div>
+
+          <div>
+          <label htmlFor="lastName"><small>Last Name</small></label>
+          <input name="firstName" type="firstName" />
+        </div>
+
         <div>
           <label htmlFor="email"><small>Email</small></label>
           <input name="email" type="text" />
@@ -23,6 +33,9 @@ const AuthForm = (props) => {
           <label htmlFor="password"><small>Password</small></label>
           <input name="password" type="password" />
         </div>
+
+
+
         <div>
           <button type="submit">{displayName}</button>
         </div>
