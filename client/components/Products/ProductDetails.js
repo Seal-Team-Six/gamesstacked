803ba8d9fe117a328302926eeb34ba3aef1d8a14
siameseguy <psyhakhom@gmail.com>
2018-06-06 12:35:52 -0400

    working on details page

--- client/components/Products/ProductDetails.js
+++ client/components/Products/ProductDetails.js
@@ -1,17 +1,69 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Grid, Image, Button} from 'semantic-ui-react'
+import {connect} from 'react-redux'
+import {fetchProduct, resetProduct} from '../../reducers/productsReducer';
 
-const ProductDetails = () => {
+class ProductDetails extends Component {
 
-  return (
-    <div>
-      <Grid.Column key={1}>
-        <Image src='https://i.ytimg.com/vi/DKbkKJWYT6E/maxresdefault.jpg'/>
-        <Button primary>Add To Cart</Button>
-      </Grid.Column>
-    </div>
-  )
+  componentDidMount() {
+    this
+      .props
+      .getProduct(this.props.match.params.id)
+  }
 
+  componentWillUnmount() {
+    this
+      .props
+      .removeProduct()
+  }
+
+  render() {
+    console.log('product details', this.props.selectedProduct)
+    if (!this.props.selectedProduct.id) {
+      return (
+        <div>..loading</div>
+      )
+    }
+    return (
+      <div>
+
+        <Grid celled>
+          <Grid.Row>
+            <Grid.Column width={16}>
+              < Image src="https://i.ytimg.com/vi/DKbkKJWYT6E/maxresdefault.jpg"/>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column width={3}>
+              < Image src="https://i.ytimg.com/vi/DKbkKJWYT6E/maxresdefault.jpg"/>
+            </Grid.Column>
+            <Grid.Column width={10}>
+              < Image src="https://i.ytimg.com/vi/DKbkKJWYT6E/maxresdefault.jpg"/>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              < Image src="https://i.ytimg.com/vi/DKbkKJWYT6E/maxresdefault.jpg"/>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+
+    )
+  }
 }
 
-export default ProductDetails
+const mapStateToProps = state => {
+  return {selectedProduct: state.products.selectedProduct}
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getProduct: (id) => {
+      dispatch(fetchProduct(id))
+    },
+    removeProduct: () => {
+      dispatch(resetProduct())
+    }
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ProductDetails)
--- client/components/Products/Products.js
+++ client/components/Products/Products.js
@@ -5,7 +5,6 @@ import ProductsList from './ProductsList';
 class Products extends React.Component {
 		render() {
 				const {products} = this.props;
-				console.log('products', products)
 				return (<ProductsList products={products}/>)
 		}
 }
