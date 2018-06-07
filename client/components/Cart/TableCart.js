b59c6370a527aa037138878f1ec2b8bd499abf44
mmcdevi1 <mmcdevi1@gmail.com>
2018-06-07 13:03:41 -0400

    table cart

--- client/components/Cart/TableCart.js
+++ client/components/Cart/TableCart.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { Table } from 'semantic-ui-react';
+import React from 'react'
+import {Table} from 'semantic-ui-react'
 
 class TableCart extends React.Component {
-	render () {
-		return (
-			<Table unstackable>
-				<Table.Header>
-		      <Table.Row>
-		        <Table.HeaderCell></Table.HeaderCell>
-		        <Table.HeaderCell>Name</Table.HeaderCell>
-		        <Table.HeaderCell>Quantity</Table.HeaderCell>
-		        <Table.HeaderCell>Remove</Table.HeaderCell>
-		        <Table.HeaderCell textAlign='right'>Price</Table.HeaderCell>
-		      </Table.Row>
-		    </Table.Header>
-		    { this.props.children }
-		  </Table>
-		)
-	}
+  render() {
+    return (
+      <Table unstackable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell />
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Quantity</Table.HeaderCell>
+            <Table.HeaderCell>Remove</Table.HeaderCell>
+            <Table.HeaderCell textAlign="right">Price</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        {this.props.children}
+      </Table>
+    )
+  }
 }
 
-export default TableCart;
+export default TableCart
