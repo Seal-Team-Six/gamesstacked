1060cce1ff63389a24440c2c3d7139ee3cdb16df
iknepper <iknepper@hotmail.com>
2018-06-07 16:49:54 -0400

    more forem reg

--- client/components/Auth/RegisterForm.js
+++ client/components/Auth/RegisterForm.js
@@ -1,60 +1,73 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 // import PropTypes from 'prop-types';
 import {auth, notGoogleRegister} from '../../reducers/store'
 import {Icon, Button, Input, List, Container, Form} from 'semantic-ui-react'
 import SocialButton from '../UI/SocialButton'
 // import Signup from '../auth-form'
+import {Field, reduxForm} from 'redux-form'
 
 /**
  * COMPONENT
  */
-const RegForm = props => {
-  const {name, displayName, handleSubmit, error} = props
 
-  return (
-    <div>
+const FIELDS = [
+  {label: 'First Name', name: 'firstName'},
+  {label: 'Last Name', name: 'lastName'},
+  {label: 'Email', name: 'email'},
+  {label: 'Password', name: 'password'},
+  {label: 'Password Confirmation', name: 'passwordConfirm'}
+]
+
+class RegForm extends Component {
+  renderField(field) {
+    const {label, type, input, meta: {error, touched}} = field
+    return (
+      <Form.Group>
+        <Input type={type} placeholder={label} {...input} />
+      </Form.Group>
+    )
+  }
+
+  render() {
+    const {name, displayName, handleSubmit, error} = this.props
+
+    return (
       <div>
-        <form onSubmit={handleSubmit} name={name}>
-          <Form.Group widths="equal">
-            <Input name="firstName" type="text" placeholder="First Name" />
-            <Input name="lastName" type="text" placeholder="Last Name" />
-          </Form.Group>
-          <Form.Group>
-            <Input name="email" type="text" placeholder="Email" />
-            <Input name="address" type="text" placeholder="Address" />
-          </Form.Group>
-
-          <Form.Group widths="equal">
-            <Input name="city" type="text" placeholder="City" />
-            <Input name="state" type="text" placeholder="State" />
-            <Input name="zipCode" type="text" placeholder="Zip Code" />
-          </Form.Group>
-          <Input name="phone" type="text" placeholder="Phone" fluid />
-          <Input name="password" type="password" placeholder="Password" fluid />
-
-          <Input
-            name="imageUrl"
-            type="text"
-            placeholder="Profile Pic Image Url"
-            fluid
-          />
-          <div>
-            <Button type="submit">{displayName}</Button>
-          </div>
-          {error && error.response && <div> {error.response.data} </div>}
-        </form>
+        <div>
+          <Container>
+            <Form
+              onSubmit={handleSubmit(this.props.onHandleSubmit.bind(this))}
+              name={name}
+            >
+              {FIELDS.map(field => {
+                return (
+                  <Field
+                    key={field.label}
+                    type="text"
+                    component={this.renderField}
+                    label={field.label}
+                    name={field.name}
+                  />
+                )
+              })}
+              <div>
+                <Button type="submit">{displayName}</Button>
+              </div>
+              {error && error.response && <div> {error.response.data} </div>}
+            </Form>
+            <SocialButton
+              href="/auth/google"
+              displayName={displayName}
+              className="google plus"
+              name="Google"
+            />
+          </Container>
+        </div>
+        <div>{/* <Signup /> */}</div>
       </div>
-
-      <div>{/* <Signup /> */}</div>
-      <SocialButton
-        href="/auth/google"
-        displayName={displayName}
-        className="google plus"
-        name="Google"
-      />
-    </div>
-  )
+    )
+  }
 }
 
 /**
@@ -75,41 +88,17 @@ const mapSignup = state => {
 
 const mapDispatch = dispatch => {
   return {
-    handleSubmit(evt) {
-      evt.preventDefault()
-      const formName = evt.target.name
-      const password = evt.target.password.value
-      const firstName = evt.target.firstName.value
-      const lastName = evt.target.lastName.value
-      const imageUrl = evt.target.imageUrl.value
-      const address = evt.target.address.value
-      const city = evt.target.city.value
-      const state = evt.target.state.value
-      const zipCode = evt.target.zipCode.value
-      const phone = evt.target.phone.value
-      const email = evt.target.email.value
-      dispatch(
-        notGoogleRegister(
-          {
-            email,
-            password,
-            firstName,
-            lastName,
-            imageUrl,
-            address,
-            city,
-            state,
-            zipCode,
-            phone
-          },
-          formName
-        )
-      )
+    onHandleSubmit(formProps) {
+      dispatch(notGoogleRegister(formProps, 'signup'))
     }
   }
 }
-
-export const RegisterForm = connect(mapSignup, mapDispatch)(RegForm)
+function validate() {}
+const form = reduxForm({
+  form: 'register',
+  validate: validate
+})(RegForm)
+export const RegisterForm = connect(mapSignup, mapDispatch)(form)
 
 /**
  * PROP TYPES
--- client/reducers/reducers.js
+++ client/reducers/reducers.js
@@ -1,8 +1,10 @@
 import user from './user'
 import products from './productsReducer'
 import cart from './cartReducer'
+import {reducer as reduxForm} from 'redux-form'
 
 export default {
+  form: reduxForm,
   user,
   products,
   cart
