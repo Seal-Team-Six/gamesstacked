73a7a74321f508f7118ae149d8967785d7547174
siameseguy <psyhakhom@gmail.com>
2018-06-07 00:26:47 -0400

    almost done with product details page

--- client/components/Products/ProductDetails.js
+++ client/components/Products/ProductDetails.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react'
-import {Grid, Container, Image, Button} from 'semantic-ui-react'
+import {
+  Grid,
+  Container,
+  Image,
+  Button,
+  Statistic,
+  Embed
+} from 'semantic-ui-react'
+import Gallery from 'react-grid-gallery'
 import {connect} from 'react-redux'
 import {fetchProduct, resetProduct} from '../../reducers/productsReducer';
-
 import {addToCart} from '../../reducers/cartReducer'
 
 class ProductDetails extends Component {
@@ -22,10 +29,35 @@ class ProductDetails extends Component {
   render() {
     const {cartId, addToCart, match: {
         params
-      }} = this.props;
+      }} = this.props
 
     const {selectedProduct} = this.props
+
+    const images = selectedProduct.screenshots && selectedProduct
+      .screenshots
+      .map(screenshot => {
+        return ({
+          id: `${screenshot.id}`,
+          src: `http://${screenshot.url}`,
+          thumbnail: `http://${screenshot.url}`,
+          thumbnailWidth: 320,
+          thumbnailHeight: 174,
+          isSelected: false
+        })
+      })
+
+    const ratings = (score) => {
+      if (score > 75) {
+        return 'green'
+      } else if (score > 50 && score < 74) {
+        return 'orange'
+      } else {
+        return 'red'
+      }
+    }
+
     console.log('!!!', selectedProduct)
+
     if (!this.props.selectedProduct.id) {
       return (
         <div>..loading</div>
@@ -41,22 +73,63 @@ class ProductDetails extends Component {
             : `http://${selectedProduct.cover.url}`}/>
         </Grid.Column>
         <Container>
-          <Grid.Row>
-            <Grid.Column className="product-cover" width={6}>
-              <Image src={`http://${selectedProduct.cover.url}`}/>
-              <Button
-                className="product-cover-button"
-                primary
-                onClick={() => addToCart(params.id, cartId)}
-                fluid
-                positive>Add To Cart
-              </Button>
-            </Grid.Column>
-            <Grid.Column className="product-content" width={8}>
-              <h1>{selectedProduct.name}</h1>
-              <p>{selectedProduct.summary}</p>
-            </Grid.Column>
-          </Grid.Row>
+          <Grid className="product-content" columns="three" divided>
+            <Grid.Row>
+              <Grid.Column computer={4}>
+                <Image className="product-cover" src={`http://${selectedProduct.cover.url}`}/>
+                <Button
+                  className="product-cover-button"
+                  primary
+                  onClick={() => addToCart(params.id, cartId)}
+                  fluid
+                  positive>Add To Cart
+                </Button>
+              </Grid.Column>
+              <Grid.Column computer={8}>
+                <h1>{selectedProduct.name}</h1>
+                {selectedProduct.summary
+                  ? <p>{selectedProduct.summary}</p>
+                  : <p>There is no summary for this game</p>}
+
+              </Grid.Column>
+              <Grid.Column className="rating" computer={4} textAlign="center">
+                <Statistic color={ratings(selectedProduct.totalRating)}>
+                  <Statistic.Value>
+                    {Math.floor(selectedProduct.totalRating) !== 0
+                      ? Math.floor(selectedProduct.totalRating)
+                      : 'No rating for this game.'
+}
+                  </Statistic.Value>
+                  <Statistic.Label>User Rating
+                  </Statistic.Label>
+                </Statistic>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+
+          <Grid.Column key={16}>
+            {selectedProduct && selectedProduct.videos
+              ? <div>
+                  <h4>Videos:</h4>
+                  <Embed
+                    id={selectedProduct.videos[0].video_id}
+                    placeholder={`http://${selectedProduct.cover.url}`}
+                    source='youtube'/>
+                </div>
+
+              : null}
+
+            {selectedProduct.screenshots && selectedProduct.screenshots.length
+              ? <div>
+                  <h4>Screenshots:</h4>
+                  <Gallery
+                    images={images}
+                    key={selectedProduct.id}
+                    backdropClosesModal={true}
+                    preloadNextImage={true}/>
+                </div>
+              : null}
+          </Grid.Column>
         </Container>
       </div>
 
@@ -66,7 +139,7 @@ class ProductDetails extends Component {
 
 const mapStateToProps = state => {
   const {cartId} = state.cart;
-  const {selectedProduct} = state.products;
+  const {selectedProduct} = state.products
 
   return {selectedProduct, cartId}
 }
