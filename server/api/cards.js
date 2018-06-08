45ce216c83e8149ed184addd348a7262dcddc880
Iggyfufu <kennethzhu07@gmail.com>
2018-06-08 15:05:36 -0400

    feat(creditcard) updated credit card models to be RESTful

--- server/api/cards.js
+++ server/api/cards.js
@@ -3,11 +3,11 @@ const {Card} = require('../db/models')
 
 module.exports = router
 
-router.get('/:userId', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const cards = await Card.findAll({
       where: {
-        userId: req.params.userId
+        userId: req.body.userId
       }
     })
     res.send(cards)
@@ -18,8 +18,38 @@ router.get('/:userId', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    await Card.create(req.body)
-    res.end()
+    const createdCard = await Card.create(req.body)
+    if (createdCard) {
+      res.sendStatus(201)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:cardId', async (req, res, next) => {
+  try {
+    const updatedCard = await Card.update(req.body, {
+      where: {
+        id: req.params.cardId
+      }
+    })
+    if (updatedCard) {
+      res.sendStatus(201)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:cardId', async (req, res, next) => {
+  try {
+    const deletedCard = await Card.destroy({
+      where: {
+        id: req.params.cardId
+      }
+    })
+    res.send(deletedCard)
   } catch (err) {
     next(err)
   }
