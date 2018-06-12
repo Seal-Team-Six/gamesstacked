41ea3a9e075f3b779bbf722e0c59d05e83084465
iknepper <iknepper@hotmail.com>
2018-06-12 13:08:41 -0400

    styled Log-in Page

---
+++ client/components/Account/Account.spec.js
@@ -0,0 +1,22 @@
+/* global describe beforeEach it */
+
+import {expect} from 'chai'
+import React from 'react'
+import enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import {Account} from './Account'
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+describe('Account', () => {
+  let userAccount
+
+  beforeEach(() => {
+    userAccount = shallow(<Account firstName="team6" />)
+  })
+
+  it('renders the email in an h3', () => {
+    expect(userAccount.find('h3').text()).to.be.equal('Welcome, team6')
+  })
+})
--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,36 +2,102 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {auth} from '../reducers/store'
-
-import {Icon, Button, Form} from 'semantic-ui-react'
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment
+} from 'semantic-ui-react'
 import SocialButton from './UI/SocialButton'
 
 /**
  * COMPONENT
  */
+
 const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props
 
   return (
-    <div>
-      <Form onSubmit={handleSubmit} name={name}>
-        <div>
-          <input name="email" type="text" placeholder="Email" />
-        </div>
-        <div>
-          <input name="password" type="password" placeholder="Password" />
-        </div>
-        <div>
-          <Button type="submit">{displayName}</Button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </Form>
-      <SocialButton
-        href="/auth/google"
-        displayName={displayName}
-        className="google plus"
-        name="Google"
-      />
+    // <div>
+    //   <Form onSubmit={handleSubmit} name={name}>
+    //     <div>
+    //       <input name="email" type="text" placeholder="Email" />
+    //     </div>
+    //     <div>
+    //       <input name="password" type="password" placeholder="Password" />
+    //     </div>
+    //     <div>
+    //       <Button type="submit">{displayName}</Button>
+    //     </div>
+    //     {error && error.response && <div> {error.response.data} </div>}
+    //   </Form>
+    //   <SocialButton
+    //     href="/auth/google"
+    //     displayName={displayName}
+    //     className="google plus"
+    //     name="Google"
+    //   />
+    // </div>
+
+    <div className="login-form">
+      {/*
+      Heads up! The styles below are necessary for the correct render of this example.
+      You can do same with CSS, the main idea is that all the elements up to the `Grid`
+      below must have a height of 100%.
+    */}
+      <style>{`
+      body > div,
+      body > div > div,
+      body > div > div > div.login-form {
+        height: 100%;
+      }
+    `}</style>
+      <Grid textAlign="center" style={{height: '100%'}} verticalAlign="middle">
+        <Grid.Column style={{maxWidth: 450}}>
+          <Header as="h2" color="teal" textAlign="center">
+            <Image src="https://cdn.onlinewebfonts.com/svg/img_332705.png" />{' '}
+            Log-in to your account
+          </Header>
+          <Form onSubmit={handleSubmit} name={name} size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                icon="user"
+                iconPosition="left"
+                placeholder="E-mail address"
+                name="email"
+              />
+              <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+                name="password"
+              />
+
+              <Button type="submit" color="teal" fluid size="large">
+                {displayName}
+              </Button>
+            </Segment>
+            {error && error.response && <div> {error.response.data} </div>}
+          </Form>
+          <div>
+            <SocialButton
+              href="/auth/google"
+              displayName={displayName}
+              className="google plus"
+              name="Google"
+            />
+          </div>
+          <Message>
+            New to us? <a href="#"> Sign Up</a>
+          </Message>
+        </Grid.Column>
+      </Grid>
     </div>
   )
 }
@@ -46,7 +112,7 @@ const AuthForm = props => {
 const mapLogin = state => {
   return {
     name: 'login',
-    displayName: 'Login',
+    displayName: 'Sign in',
     error: state.user.error
   }
 }
