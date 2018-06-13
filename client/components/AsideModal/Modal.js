47edf0d1c0c95829f1e4f6ed02d94e1f80e2abcb
siameseguy <psyhakhom@gmail.com>
2018-06-13 11:08:22 -0400

    cleanup(linter):fixing lint issues

--- client/components/AsideModal/Modal.js
+++ client/components/AsideModal/Modal.js
@@ -3,25 +3,23 @@ import {connect} from 'react-redux'
 
 import Backdrop from './Backdrop'
 
-class AsideModal extends React.Component {
-  render() {
-    const {back, showModal, children} = this.props
-    const classes = showModal ? 'open' : 'close'
+const AsideModal = props => {
+  const {back, showModal, children} = props
+  const classes = showModal ? 'open' : 'close'
 
-    return (
-      <div>
-        <Backdrop back={back} showModal={showModal} />
-        <div className={`aside-modal ${classes}`}>
-          <div onClick={back} className="cancel-form">
-            <button type="button" className="md-btn md-fab mb-2 white">
-              X
-            </button>
-          </div>
-          {children}
+  return (
+    <div>
+      <Backdrop back={back} showModal={showModal} />
+      <div className={`aside-modal ${classes}`}>
+        <div onClick={back} className="cancel-form">
+          <button type="button" className="md-btn md-fab mb-2 white">
+            X
+          </button>
         </div>
+        {children}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 function mapStateToProps(state) {
--- client/routes.js
+++ client/routes.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
@@ -15,38 +15,33 @@ import AuthenticateAdmin from './hoc/AdminAuthentication'
 import AdminRoutes from './components/Admin/AdminRoutes'
 import SingleOrderNonAdmin from './components/Admin/Orders/SingleOrderNonAdmin'
 
-/**
- * COMPONENT
- */
-class Routes extends Component {
-  render() {
-    const {isLoggedIn} = this.props
+const Routes = props => {
+  const {isLoggedIn} = props
 
-    return (
-      <Switch>
-        {/* Routes placed here are available to all visitors */}
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={RegisterForm} />
-        <Route exact path="/product/:id" component={ProductDetails} />
-        <Route exact path="/products" component={Products} />
-        <Route exact path="/cart" component={Cart} />
-        <Route exact path="/checkout" component={CheckoutContainer} />
+  return (
+    <Switch>
+      {/* Routes placed here are available to all visitors */}
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={RegisterForm} />
+      <Route exact path="/product/:id" component={ProductDetails} />
+      <Route exact path="/products" component={Products} />
+      <Route exact path="/cart" component={Cart} />
+      <Route exact path="/checkout" component={CheckoutContainer} />
 
-        {/* Routes placed here are only available after logging in */}
-        {isLoggedIn && (
-          <Switch>
-            <Route exact path="/user" component={Account} />
-            <Route exact path="/checkout" component={CheckoutContainer} />
-            <Route exact path="/thankyou" component={ThankYou} />
-            <Route path="/admin" component={AuthenticateAdmin(AdminRoutes)} />
-            <Route path="/orders/:id" component={SingleOrderNonAdmin} />
-            <Route component={Products} />
-          </Switch>
-        )}
-        <Route component={Products} />
-      </Switch>
-    )
-  }
+      {/* Routes placed here are only available after logging in */}
+      {isLoggedIn && (
+        <Switch>
+          <Route exact path="/user" component={Account} />
+          <Route exact path="/checkout" component={CheckoutContainer} />
+          <Route exact path="/thankyou" component={ThankYou} />
+          <Route path="/admin" component={AuthenticateAdmin(AdminRoutes)} />
+          <Route path="/orders/:id" component={SingleOrderNonAdmin} />
+          <Route component={Products} />
+        </Switch>
+      )}
+      <Route component={Products} />
+    </Switch>
+  )
 }
 
 /**
